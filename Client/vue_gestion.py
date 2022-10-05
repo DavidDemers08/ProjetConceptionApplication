a1d276a43f314c26e9d87601a8a20c247cd647c8
elperrrucho <eddygarcia06@gmail.com>
2022-10-05 12:39:47 -0400

    Mmodule de gestion maquette finie, reste les fonctions display des boutons

--- Client/vue_gestion.py
+++ Client/vue_gestion.py
@@ -16,7 +16,10 @@ class VueGestion(ttk.Frame):
 
     def remplir_vue_gestion(self):
 
-        self.data =[1,2,3,4]
+        self.data =("1","2","3","4")
+        self.data1 = ("allo", "bigg","toast")
+        #self.listWidt
+          #  =int(self.winfo_width()/3)
         self.bouton_gestion_membre = ttk.Button(self, text='Gestion Membre', command=self.clic_bouton_membre)
         self.bouton_gestion_membre.grid(row=0, column=0, pady=(20, 0), sticky=tk.E)
 
@@ -26,36 +29,84 @@ class VueGestion(ttk.Frame):
         self.bouton_gestion_modules = ttk.Button(self, text='Gestion modules', command=self.clic_bouton_modules)
         self.bouton_gestion_modules.grid(row=0, column=2, pady=(20, 0), sticky=tk.E)
 
-        self.canevas_list = tk.Canvas(self,width=200,height=200, bg= 'white')
-        self.canevas_list.grid(row = 1, column=0, columnspan=3, sticky=tk.S)
+        self.canevas_list = tk.Canvas(self,height=200, bg= 'white')
 
-        self.list = tk.Listbox(self, height=5, width=5)
-        for i in self.data:
-            self.list.insert(i)
-
-        self.canevas_list.create_window(0, 0, window=self.list, width=200, height=200)
+        self.canevas_list.grid(row = 1, column=0, columnspan=3, sticky=tk.E)
+        self.lWidth = int(self.canevas_list.winfo_width() /3)
 
 
+        self.bouton_ajouter_membre = ttk.Button(self, text='Ajouter Membre', command=self.clic_bouton_ajout_membre)
+        self.bouton_ajouter_membre.grid(row=2, column=0, pady=(20, 0),sticky=tk.E)
 
-        """
-        self.bouton_connexion.bind('<Return>', lambda e: self.bouton_connexion.invoke())
-        self.bouton_connexion.grid(row=3, column=1, pady=(20, 0), sticky=tk.E)
+        self.bouton_gerer_employe = ttk.Button(self, text='Gerer Employe', command=self.clic_bouton_gestion_employe)
+        self.bouton_gerer_employe.grid(row=2, column=2, pady=(20, 0), sticky=tk.E)
+        #self.canevas_list.create_window(0, 0, window=self.list, width=200, height=200)
 
-        self.bouton_annuler = ttk.Button(self, text='Annuler', command=self.clic_bouton_annuler)
-        self.bouton_annuler.bind('<Return>', lambda e: self.bouton_annuler.invoke())
-        self.bouton_annuler.grid(row=4, column=1, pady=(10, 0), sticky=tk.E)
 
-        self.label_message = ttk.Label(self, text='', foreground='red')
-        self.label_message.grid(row=5, column=0, columnspan=2, sticky=tk.W)
-       """
+
+    def delete_lists(self):
+        self.canevas_list.destroy()
+        self.canevas_list = tk.Canvas(self, height=200, bg='white')
+        self.canevas_list.grid(row=1, column=0, columnspan=3, sticky=tk.E)
 
     def clic_bouton_membre(self):
-        pass
-    def clic_bouton_projets(self):
+        self.delete_lists()
+        print(self.canevas_list.winfo_width()+1)
+        self.list_identifiant = tk.Listbox(self.canevas_list, selectmode='browse' )
+        self.list_permission = tk.Listbox(self.canevas_list, selectmode='browse' )
+        self.list_role = tk.Listbox(self.canevas_list, selectmode='browse' )
+
+        for num in self.data:
+            self.list_identifiant.insert(END, num)
+        self.list_identifiant.place(x=25, y=0)
+
+        for num in self.data1:
+            self.list_permission.insert(END, num)
+        self.list_permission.place(x=125, y=0)
+
+        for num in self.data:
+            self.list_role.insert(END, num)
+        self.list_role.place(x=225, y=0)
+
+    def populate_list(self):
         pass
 
-    def clic_bouton_modules(self):
+    def clic_bouton_ajout_membre(self):
         pass
+
+    def clic_bouton_gestion_employe(self):
+        pass
+    def clic_bouton_projets(self):
+        #self.canevas_list.delete(self)
+        self.delete_lists()
+        #self.canevas_list.after(1000)
+
+        self.list_identifiant = tk.Listbox(self.canevas_list, selectmode='browse')
+        self.list_permission = tk.Listbox(self.canevas_list, selectmode='browse')
+        self.list_role = tk.Listbox(self.canevas_list, selectmode='browse')
+
+        for num in self.data1:
+            self.list_identifiant.insert(END, num)
+        self.list_identifiant.place(x=25, y=0)
+
+        for num in self.data:
+            self.list_permission.insert(END, num)
+        self.list_permission.place(x=125, y=0)
+
+        for num in self.data:
+            self.list_role.insert(END, num)
+        self.list_role.place(x=225, y=0)
+
+
+    def clic_bouton_modules(self):
+        # self.canevas_list.delete(self)
+        self.delete_lists()
+        # self.canevas_list.after(1000)
+        self.list_role = tk.Listbox(self.canevas_list, selectmode='browse')
+
+        for num in self.data:
+            self.list_role.insert(END, num)
+        self.list_role.place(x=0, y=0)
 
     def clic_bouton_annuler(self):
         self.var_nom.set('')
