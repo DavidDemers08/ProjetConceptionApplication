839628dadee08ff132e402ce1ce8a0567db3738c
Carlens Belony <carlens2000@live.ca>
2022-10-19 11:32:09 -0400

    module gestion employe

--- Client/gestion_main.py
+++ Client/gestion_main.py
@@ -1,18 +1,21 @@
 from traceback import print_exc
 from tkinter import Tk
 from vue_gestion import VueGestion
+from vue_gerer_emp import VueGererEmp
 from controleur_client import Controleur_Client
 
 class Module_Gestion(Tk):
     def __init__(self):
         super().__init__()
-        controleur = Controleur_Client()
+        self.controleur = Controleur_Client()
 
         # peut-être éventuellement dans une sous-classe
         vue_gestion = VueGestion(self)
         vue_gestion.grid(row=3, column=3, padx=10, pady=10)
-        vue_gestion.set_controleur(controleur)
-        controleur.set_vue_gestion(vue_gestion)
+        vue_gestion.set_controleur(self.controleur)
+        self.controleur.set_vue_gestion(vue_gestion)
+
+
 
 def main():
     try:
