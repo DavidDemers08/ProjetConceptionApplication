f180f06b9fdacb7c967df3506ad17b2a27584290
1379916 <golfjob565@gmail.com>
2022-10-26 10:51:26 -0400

    Module paiement + vue

---
+++ Client/module_paiement.py
@@ -0,0 +1,31 @@
+from tkinter import Tk
+from traceback import print_exc
+
+from Client.vue_gestion import VueGestion
+from vue_paiement import VuePaiement
+from controleur_client import Controleur_Client
+
+
+class ModulePaiement(Tk):
+    def __init__(self):
+        super().__init__()
+        controleur = Controleur_Client()
+
+        vue_paiement = VuePaiement(self)
+        vue_paiement.grid(row=3, column=3, padx=10, pady=10)
+        vue_paiement.set_controleur(controleur)
+        controleur.set_vue(vue_paiement)
+
+
+def main():
+    try:
+        module = ModulePaiement()
+        module.mainloop()
+    except:
+        print_exc()
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    quit(main())
