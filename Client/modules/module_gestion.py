67cf4810acee3ec4b7cd829cb0edecd0849fb960
1942878 <e.ddemers3@etu.cvm.qc.ca>
2022-11-16 08:14:12 -0500

    david

--- Client/modules/module_gestion.py
+++ Client/modules/module_gestion.py
@@ -1,12 +1,7 @@
 from tkinter import Tk
 
-<<<<<<< HEAD
-# from Client.controleur_client import Controleur_Client
-=======
-#from Client.controleur_client import Controleur_Client
 from traceback import print_exc
 
->>>>>>> 8273f25fa2c8c24be2205c360ab409a3067c7bd4
 from Client.vues.vue_gestion import VueGestion
 
 
@@ -18,8 +13,6 @@ class ModuleGestion(Tk):
         vue = VueGestion(self, controleur)
         vue.grid(row=0, column=0, padx=10, pady=10)
         parent.set_vue(vue)
-<<<<<<< HEAD
-=======
         print(controleur.username)
 
 
@@ -36,4 +29,3 @@ def main():
 
 if __name__ == '__main__':
     quit(main())
->>>>>>> 8273f25fa2c8c24be2205c360ab409a3067c7bd4
