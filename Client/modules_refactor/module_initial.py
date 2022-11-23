43d1d8c25a767f72ec7ab28ad56bf62be48adc65
JcCaron <Caronjc00@gmail.com>
2022-11-23 10:06:37 -0500

    refactor

--- Client/modules_refactor/module_initial.py
+++ Client/modules_refactor/module_initial.py
@@ -11,47 +11,46 @@ import Utils.utils
 
 class ModuleInitial(Module):
 
-    def __init__(self, parent):
-        super().__init__(parent)
-        # controleur.creation_modules()
+    def __init__(self, controleur, master_frame):
+        super().__init__(controleur, master_frame)
 
     def set_vue(self):
-        return ModuleInitial.VueInitial(self, row=0, column=0, padx=10, pady=10)
+        return ModuleInitial.VueInitial(self, self.master_frame, row=0, column=0, padx=10, pady=10)
 
     class VueInitial(Vue):
-        def __init__(self, parent, row: int, column: int, padx: int, pady: int):
-            super().__init__(parent, row, column, padx, pady)
-            # print(self.controleur.creation_modules())
+        def __init__(self, parent, master_frame, row: int, column: int, padx: int, pady: int):
+            super().__init__(parent, master_frame, row, column, padx, pady)
+
 
         def remplir_vue(self):
-            self.label_nom = ttk.Label(self, text='Nom ')
+            self.label_nom = ttk.Label(self.master_frame, text='Nom ')
             self.label_nom.grid(row=1, column=0, pady=(5, 0), sticky=tk.E)
 
             self.var_nom = tk.StringVar()
-            self.input_nom = ttk.Entry(self, textvariable=self.var_nom, width=30)
+            self.input_nom = ttk.Entry(self.master_frame, textvariable=self.var_nom, width=30)
             self.input_nom.grid(row=1, column=1, sticky=tk.E)
 
-            self.label_mdp = ttk.Label(self, text='Mot de passe ')
+            self.label_mdp = ttk.Label(self.master_frame, text='Mot de passe ')
             self.label_mdp.grid(row=2, column=0, pady=(5, 0), sticky=tk.E)
 
             self.var_mdp = tk.StringVar()
-            self.input_mdp = ttk.Entry(self, textvariable=self.var_mdp, show='*', width=30)
+            self.input_mdp = ttk.Entry(self.master_frame, textvariable=self.var_mdp, show='*', width=30)
             self.input_mdp.grid(row=2, column=1, sticky=tk.E)
 
-            self.bouton_connexion = ttk.Button(self, text='Connexion', command=self.clic_bouton_connexion)
+            self.bouton_connexion = ttk.Button(self.master_frame, text='Connexion', command=self.clic_bouton_connexion)
             self.bouton_connexion.bind('<Return>', lambda e: self.bouton_connexion.invoke())
             self.bouton_connexion.grid(row=3, column=1, pady=(20, 0), sticky=tk.E)
 
-            self.bouton_annuler = ttk.Button(self, text='Annuler', command=self.clic_bouton_annuler)
+            self.bouton_annuler = ttk.Button(self.master_frame, text='Annuler', command=self.clic_bouton_annuler)
             self.bouton_annuler.bind('<Return>', lambda e: self.bouton_annuler.invoke())
             self.bouton_annuler.grid(row=4, column=1, pady=(10, 0), sticky=tk.E)
 
-            self.bouton_enregister = ttk.Button(self, text='Enregistrer une nouvelle compagnie',
+            self.bouton_enregister = ttk.Button(self.master_frame, text='Enregistrer une nouvelle compagnie',
                                                 command=self.clic_bouton_enregister)
             self.bouton_enregister.bind('<Return>', lambda e: self.bouton_enregister.invoke())
             self.bouton_enregister.grid(row=5, column=1, pady=(10, 0), sticky=tk.E)
 
-            self.label_message = ttk.Label(self, text='', foreground='red')
+            self.label_message = ttk.Label(self.master_frame, text='', foreground='red')
             self.label_message.grid(row=5, column=0, columnspan=2, sticky=tk.W)
 
         def clic_bouton_connexion(self):
@@ -93,40 +92,40 @@ class ModuleInitial(Module):
             self.label_message['text'] = ''
 
         def fonction_facile_a_faire_vraiment(self, label, input, texte):
-            label = ttk.Label(self, text=texte)
+            label = ttk.Label(self.master_frame, text=texte)
             label.grid(row=1, column=0, pady=(5, 0), sticky=tk.E)
 
             var = tk.StringVar()
-            input = ttk.Entry(self, textvariable=var, width=30)
+            input = ttk.Entry(self.master_frame, textvariable=var, width=30)
             input.grid(row=1, column=1, sticky=tk.E)
             return
 
         def afficher_enregistrer(self):
-            self.label_nom_compagnie = ttk.Label(self, text='Nom Compagnie ')
+            self.label_nom_compagnie = ttk.Label(self.master_frame, text='Nom Compagnie ')
             self.label_nom_compagnie.grid(row=1, column=0, pady=(5, 0), sticky=tk.E)
             self.var_nom_compagnie = tk.StringVar()
-            self.input_nom_compagnie = ttk.Entry(self, textvariable=self.var_nom_compagnie, width=30)
+            self.input_nom_compagnie = ttk.Entry(self.master_frame, textvariable=self.var_nom_compagnie, width=30)
             self.input_nom_compagnie.grid(row=1, column=1, sticky=tk.E)
 
-            self.label_pays = ttk.Label(self, text='pays ')
+            self.label_pays = ttk.Label(self.master_frame, text='pays ')
             self.label_pays.grid(row=2, column=0, pady=(5, 0), sticky=tk.E)
             self.var_pays = tk.StringVar()
-            self.input_pays = ttk.Entry(self, textvariable=self.var_pays, width=30)
+            self.input_pays = ttk.Entry(self.master_frame, textvariable=self.var_pays, width=30)
             self.input_pays.grid(row=2, column=1, sticky=tk.E)
 
-            self.label_province = ttk.Label(self, text='province/etat ')
+            self.label_province = ttk.Label(self.master_frame, text='province/etat ')
             self.label_province.grid(row=3, column=0, pady=(5, 0), sticky=tk.E)
             self.var_province = tk.StringVar()
-            self.input_province = ttk.Entry(self, textvariable=self.var_province, width=30)
+            self.input_province = ttk.Entry(self.master_frame, textvariable=self.var_province, width=30)
             self.input_province.grid(row=3, column=1, sticky=tk.E)
 
-            self.label_region = ttk.Label(self, text='region')
+            self.label_region = ttk.Label(self.master_frame, text='region')
             self.label_region.grid(row=4, column=0, pady=(5, 0), sticky=tk.E)
             self.var_region = tk.StringVar()
-            self.input_region = ttk.Entry(self, textvariable=self.var_region, width=30)
+            self.input_region = ttk.Entry(self.master_frame, textvariable=self.var_region, width=30)
             self.input_region.grid(row=4, column=1, sticky=tk.E)
 
-            self.bouton_enregister_ville = ttk.Button(self, text='Enregistrer',
+            self.bouton_enregister_ville = ttk.Button(self.master_frame, text='Enregistrer',
                                                       command=self.clic_bouton_enregistrer_ville)
             self.bouton_enregister_ville.bind('<Return>', lambda e: self.bouton_enregister_ville.invoke())
             self.bouton_enregister_ville.grid(row=5, column=1, pady=(10, 0), sticky=tk.E)
@@ -141,43 +140,43 @@ class ModuleInitial(Module):
                 print(self.controleur.afficher_compagnies())
 
         def afficher_enregistrement_admin(self):
-            self.label_uti_admin = ttk.Label(self, text='Nom utilisateur ')
+            self.label_uti_admin = ttk.Label(self.master_frame, text='Nom utilisateur ')
             self.label_uti_admin.grid(row=1, column=0, pady=(5, 0), sticky=tk.E)
             self.var_uti_admin = tk.StringVar()
-            self.input_uti_admin = ttk.Entry(self, textvariable=self.var_uti_admin, width=30)
+            self.input_uti_admin = ttk.Entry(self.master_frame, textvariable=self.var_uti_admin, width=30)
             self.input_uti_admin.grid(row=1, column=1, sticky=tk.E)
 
-            self.label_mdp_admin = ttk.Label(self, text='Mot de passe ')
+            self.label_mdp_admin = ttk.Label(self.master_frame, text='Mot de passe ')
             self.label_mdp_admin.grid(row=2, column=0, pady=(5, 0), sticky=tk.E)
             self.var_mdp_admin = tk.StringVar()
-            self.input_mdp_admin = ttk.Entry(self, textvariable=self.var_mdp_admin, show="*", width=30)
+            self.input_mdp_admin = ttk.Entry(self.master_frame, textvariable=self.var_mdp_admin, show="*", width=30)
             self.input_mdp_admin.grid(row=2, column=1, sticky=tk.E)
 
-            self.label_verif_mdp_admin = ttk.Label(self, text='Vérifier le mot de passe ')
+            self.label_verif_mdp_admin = ttk.Label(self.master_frame, text='Vérifier le mot de passe ')
             self.label_verif_mdp_admin.grid(row=3, column=0, pady=(5, 0), sticky=tk.E)
             self.var_verif_mdp_admin = tk.StringVar()
-            self.input_verif_mdp_admin = ttk.Entry(self, textvariable=self.var_verif_mdp_admin, show="*", width=30)
+            self.input_verif_mdp_admin = ttk.Entry(self.master_frame, textvariable=self.var_verif_mdp_admin, show="*", width=30)
             self.input_verif_mdp_admin.grid(row=3, column=1, sticky=tk.E)
 
-            self.label_nom_admin = ttk.Label(self, text='Nom ')
+            self.label_nom_admin = ttk.Label(self.master_frame, text='Nom ')
             self.label_nom_admin.grid(row=4, column=0, pady=(5, 0), sticky=tk.E)
             self.var_nom_admin = tk.StringVar()
-            self.input_nom_admin = ttk.Entry(self, textvariable=self.var_nom_admin, width=30)
+            self.input_nom_admin = ttk.Entry(self.master_frame, textvariable=self.var_nom_admin, width=30)
             self.input_nom_admin.grid(row=4, column=1, sticky=tk.E)
 
-            self.label_prenom_admin = ttk.Label(self, text='Prenom ')
+            self.label_prenom_admin = ttk.Label(self.master_frame, text='Prenom ')
             self.label_prenom_admin.grid(row=5, column=0, pady=(5, 0), sticky=tk.E)
             self.var_prenom_admin = tk.StringVar()
-            self.input_prenom_admin = ttk.Entry(self, textvariable=self.var_prenom_admin, width=30)
+            self.input_prenom_admin = ttk.Entry(self.master_frame, textvariable=self.var_prenom_admin, width=30)
             self.input_prenom_admin.grid(row=5, column=1, sticky=tk.E)
 
-            self.label_genre_admin = ttk.Label(self, text='Genre Admin (H/F)')
+            self.label_genre_admin = ttk.Label(self.master_frame, text='Genre Admin (H/F)')
             self.label_genre_admin.grid(row=6, column=0, pady=(5, 0), sticky=tk.E)
             self.var_genre_admin = tk.StringVar()
-            self.input_genre_admin = ttk.Entry(self, textvariable=self.var_genre_admin, width=30)
+            self.input_genre_admin = ttk.Entry(self.master_frame, textvariable=self.var_genre_admin, width=30)
             self.input_genre_admin.grid(row=6, column=1, sticky=tk.E)
 
-            self.bouton_enregister_admin = ttk.Button(self, text='Enregistrer Admin',
+            self.bouton_enregister_admin = ttk.Button(self.master_frame, text='Enregistrer Admin',
                                                       command=self.clic_bouton_enregistrer_admin)
             self.bouton_enregister_admin.bind('<Return>', lambda e: self.bouton_enregister_admin.invoke())
             self.bouton_enregister_admin.grid(row=7, column=1, pady=(10, 0), sticky=tk.E)
